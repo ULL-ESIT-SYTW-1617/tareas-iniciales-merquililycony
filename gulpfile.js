5e10ee51d98af7c4bae961fed8771727253bcb80
Alu0100673647 <alu0100673647@ull.edu.es>
2016-10-08 18:25:00 +0100

    cambiando gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,36 +4,20 @@
 var gulp  = require('gulp');
 var shell = require('gulp-shell');
 
-var desplegargh = function() {
-  "use strict";
-  let gh = require('gh-pages');
 
-  //process.env.CMDDESC="Deploy GitBook on Github";
+gulp.task('deploy', function () {
+  return gulp.src('').pipe(shell(["./scripts/deploy-gitbook"]));
+});
 
-  let json = require('./package.json');
-  let REPO = json.repository.url;
-  console.log( "Repositorio:"+REPO);
 
-  gh.publish('./gh-pages', { repo: REPO, logger: function(m) { console.error(m); } });
-};
 
-//  "deploy-gitbook": "./scripts/losh deploy-gitbook",
-gulp.task('despliegue', desplegargh);
-
-gulp.task('empujar', ['build'], 
- shell.task(
-    "git add ."+
-    ";"+
-    "git commit -am 'deploy to github'"+
-    ";"+
-    "git push origin master",
-    { verbose: true }
-  )
-);
-
+/*gulp.task('prueba',function() {
+   return gulp.src('').pipe(shell(['gitbook build `pwd` ./gh-pages']));
+});
+*/
 
 //generar pdf
-gulp.task('pdf', 
+gulp.task('pdf',
   shell.task(
     "gitbook pdf ./txt",
     { verbose: true })
